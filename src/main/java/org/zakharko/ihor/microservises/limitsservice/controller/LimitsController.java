e570f9374682975f5c428cfbb3ca6f4e6bb463e5
Ihor Zakharko <igor.zaharko@gmail.com>
2023-02-03 12:35:13 +0200

    step 3

--- src/main/java/org/zakharko/ihor/microservises/limitsservice/controller/LimitsController.java
+++ src/main/java/org/zakharko/ihor/microservises/limitsservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package org.zakharko.ihor.microservises.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zakharko.ihor.microservises.limitsservice.configuration.Configuration;
 import org.zakharko.ihor.microservises.limitsservice.dto.Limits;
 
 @RestController
 public class LimitsController {
 
+	@Autowired
+	private Configuration configuration;
+
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1, 1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
 }
